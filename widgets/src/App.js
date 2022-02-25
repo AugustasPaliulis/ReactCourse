7ebbec7fb2cb1ac144702c76925752d89fb79db3
AugustasPaliulis <Augustas.paliulis@outlook.com>
2022-02-25 20:20:21 +0200

    Basic routing

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -35,20 +35,47 @@ const options = [
   },
 ];
 
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
+
 export default () => {
   const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropDown] = useState(true);
 
   return (
     <div>
       {/* <Accordion items={items} /> */}
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
       {/* <Search /> */}
       {/* <Dropdown
         onSelectedChange={setSelected}
         selected={selected}
         options={options}
       /> */}
-      <Translate />
+      {/* <Translate /> */}
     </div>
   );
 };
