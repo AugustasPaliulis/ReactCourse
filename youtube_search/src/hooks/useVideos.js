2f2da8f404a4f7826f3471616e35b0b63baf8a6a
AugustasPaliulis <Augustas.paliulis@outlook.com>
2022-02-28 20:12:07 +0200

    Custom hook

---
+++ youtube_search/src/hooks/useVideos.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+import Youtube from "../APIS/Youtube";
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
+
+  const search = async (term) => {
+    const response = await Youtube.get("/search", { params: { q: term } });
+
+    setVideos(response.data.items);
+  };
+
+  return [videos, search];
+};
+
+export default useVideos;
